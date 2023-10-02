c7733d2122ff6f4b1166f4aad676916151bbd032
jnieblas <53878113+jnieblas@users.noreply.github.com>
2023-10-02 15:32:03 -0500

    Adding more ErrorHandling examples

--- src/main/scala/ErrorHandlingPractice.scala
+++ src/main/scala/ErrorHandlingPractice.scala
@@ -1,15 +1,30 @@
 object ErrorHandlingPractice {
-  private def makeInt(s: String): Option[Int] = {
-    try {
+  private def makeInt(s: String): Option[Int] =
+    try
       Some(Integer.parseInt(s))
-    } catch {
+    catch
       case e: Exception => None
-    }
-  }
 
-  def main(args: Array[String]): Unit = {
-    println(ErrorHandlingPractice.makeInt("123"))
-    println(ErrorHandlingPractice.makeInt("not a num"))
-    println(ErrorHandlingPractice.makeInt("not a num but contains 1"))
-  }
+  private def printInt(i: Option[Int]): Unit =
+    i match
+      case Some(i) => println(i)
+      case None => println("not a num")
+
+  def main(args: Array[String]): Unit =
+    printInt(makeInt("123"))
+    printInt(makeInt("not a num"))
+    printInt(makeInt("not a num but contains 1"))
+
+    val string1 = "1"
+    val string2 = "2"
+    val string3 = "3"
+
+    val y = for
+      a <- makeInt(string1)
+      b <- makeInt(string2)
+      c <- makeInt(string3)
+    yield
+      a + b + c
+
+    printInt(y)
 }
